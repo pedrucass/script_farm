10bdb087792b482368bb3920b5f4752eef53d709
Pedro <pedroludmillo@gmail.com>
2025-06-11 21:02:02 -0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "cerulean"
+game "gta5"
+
+shared_script "@vrp/lib/utils.lua"
+
+client_scripts {
+    "client.lua",
+    "config.lua"
+}
+
+server_scripts {
+    "server.lua",
+    "config.lua"
+}
